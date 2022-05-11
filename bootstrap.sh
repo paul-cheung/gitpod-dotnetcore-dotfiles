90789759112b88f12487e24ffebbab59b9e8715f
paul cheung <paul_cheung@msn.cn>
2022-05-11 13:19:32 +0000

    setup path variable for dotnet

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,3 @@
 mkdir -p /tmp/dotnet && curl -fsSL https://dot.net/v1/dotnet-install.sh | bash /dev/stdin --channel 6.0 --install-dir /tmp/dotnet
 dotnet dev-certs https
+export PATH=$PATH:/tmp/dotnet
